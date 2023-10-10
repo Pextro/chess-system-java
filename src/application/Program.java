02aa2f4b59648d997b4270adebcd77cdf76ef39c
Pedro <pedrovictorima@hotmail.com>
2023-10-10 14:01:39 -0300

    Adição da classe posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardgame.position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		position pos = new position(10, 5);
+		System.out.println(pos);
 
 	}
-
 }
---
+++ src/boardgame/position.java
@@ -0,0 +1,39 @@
+package boardgame;
+
+public class position {
+
+	private int row;
+	private int column;
+
+	public position() {
+	}
+
+	public position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public void setValue(int row, int column) {
+
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	public String toString() {
+		return row + ", " + column;
+	}
+}
